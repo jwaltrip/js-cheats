d1a2aef40c22f51efb13dba223707fffd7e03ebf
Zeke <zgutier5@gmail.com>
2019-02-07 18:45:24 -0600

    updates 2/7
    added search bar globally.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,12 +3,14 @@ import React from 'react';
 
 
 class App extends Component {
+    
     render() {
-        <form class="form-inline">
+        return 
+        (<form class="form-inline">
         <div class="md-form my-0">
           <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"></input>
         </div>
-      </form>
+      </form>)
     }
   }
   
