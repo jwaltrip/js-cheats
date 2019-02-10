9db012eb6f3e202f15aa00c02cbb2b1a98bb2186
sarahcheatham <sarah.cheatham14@gmail.com>
2019-02-10 17:43:28 -0600

    added images for cards

--- src/components/HomeCard/HomeCard.js
+++ src/components/HomeCard/HomeCard.js
@@ -1,16 +1,30 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+import strings from './notebook.gif';
+import arrays from './arraysNotebook.gif';
+import numbers from './numbersNotebook.gif';
+import "./HomeCard.css";
 import { Card, CardText, CardBody, CardTitle } from 'reactstrap';
 
 const HomeCard = (props) => {
-  const { title, description } = props;
+  const { title, description, cardType } = props;
+
+  let whatToShow = "";
+  if(props.title === "strings"){
+    whatToShow = <img src={strings} alt="strings" width="275px" display="flex"/>;
+  } else if(props.title === "arrays"){
+    whatToShow = <img src={arrays} alt="arrays" width="275px" display="flex"/>;
+  } else {
+    whatToShow = <img src={numbers} alt="numbers" width="275px" display="flex"/>;
+  }
 
   return (
-    <Card>
+    <Card className="cards">
       <CardBody>
-        <CardTitle>{title}</CardTitle>
+        {whatToShow}
+        <CardTitle className="cardTitle">{title}</CardTitle>
         {/*<CardSubtitle>Card subtitle</CardSubtitle>*/}
-        <CardText>{description}</CardText>
+        <CardText classname="cardText">{description}</CardText>
       </CardBody>
     </Card>
   );
