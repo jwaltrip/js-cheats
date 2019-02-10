ca599791fdf6e02b878b1db76c4ace3fd6ae9509
Jake Waltrip <jake.waltrip@gmail.com>
2019-02-10 16:19:52 -0600

    created initial homepage links for string, array, and numbers

---
+++ src/components/ArrayPage/ArrayPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class ArrayPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Arrays</h1>
+        <hr/>
+      </div>
+    );
+  }
+}
+
+export default ArrayPage;
